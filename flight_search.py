abd2a5ea364831c1321691a08169efe6d7416e49
jsanagustin828 <121126195+jsanagustin828@users.noreply.github.com>
2024-05-16 07:32:06 -0700

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -25,15 +25,6 @@ tequila_headers = {
     "apikey": tequila_api_key,
 }
 
-
-# Tequila Get Payload
-# tequila_get_payload = {
-#     'fly_from': fly_from,
-#     'date_from': date_tomorrow,
-#     'date_to': date_180_days,
-#     'limit': 5,
-# }
-
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self):
@@ -46,7 +37,6 @@ class FlightSearch:
     def get_search_itineraries(self, sheet_data):
         flight_deal_info = []
         for num in sheet_data:
-            # print(f"We are iterating through: {num['iataCode']}")
             self.payload_dict = {
                 'fly_from': 'LAX',
                 'fly_to': num['iataCode'],
@@ -58,24 +48,16 @@ class FlightSearch:
                 'limit': 5,
             }
             response = requests.get(url=self.endpoint, params=self.payload_dict, headers=self.header_dict, verify=False)
-            # print(f"The response code for searching itineraries is: {response.status_code}")
             data = response.json()
 
             # Checking if there was any valid data for the tequila get request. It might return no data.
             if len(data['data']) > 0:
-                # print(f"All of the data printed: {data}")
-                # print(f"This is the entire data from tequila: {data['data'][0]['flyFrom']}")
-
-                # I'm pulling out the key values to get location from, to and flight price.
                 location_from = data['data'][0]['flyFrom']
-                # print(f"location from: {location_from}")
                 location_to = data['data'][0]['flyTo']
                 city_from = data['data'][0]['cityFrom']
                 city_to = data['data'][0]['cityTo']
                 flight_price = data['data'][0]['price']
-                # print(f"The flight price from {location_from} to {location_to} is ${flight_price}.")
 
-                # I'm adding the location to and flight price to a dictionary.
                 flight_deal_dict = {
                     'location_from': location_from,
                     'location_to': location_to,
@@ -88,22 +70,11 @@ class FlightSearch:
             else:
                 print(f"No data for this iteration")
 
-            # print(f"Here is the dictionary to compare for the new function: {flight_deal_info}")
-
-            # This will just break, so we don't have sit through the for loop.
-            # if response.status_code == 200:
-            #     break
         return flight_deal_info
 
     def check_if_price_is_lower(self, prices_from_spreadsheet, prices_from_tequila):
         city_prices_to_email = [tequila_flight_info for tequila_flight_info, price_spreadsheet
                                 in zip(prices_from_tequila, prices_from_spreadsheet)
                                 if tequila_flight_info['flight_price'] < price_spreadsheet]
-        # for tequila_flight_info in prices_from_tequila:
-        #     for price_spreadsheet in prices_from_spreadsheet:
-        #         if tequila_flight_info['flight_price'] < price_spreadsheet:
-                    # print(f"Tequila founder a cheaper flight for {city} that costs ${tequila_flight_info}")
-                    # city_prices_to_email[city] = tequila_flight_info
-                    # break
         return city_prices_to_email
 
